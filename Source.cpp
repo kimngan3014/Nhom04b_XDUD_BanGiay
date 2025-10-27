81c5c7393269824115930e4eb3bc57aec561a408
Nhat Huy <wywywyz123@gmail.com>
2025-10-27 11:45:53 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b){
-
-	return a + b;
-
-}
+int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b){
-	return a*b;
-}
+int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
-	cout<<"tong = " << tong(5,6);
-	cout<<"tich= "<<tich(5,6);
 	system("pause");
 	return 0;
 }
-
-	
